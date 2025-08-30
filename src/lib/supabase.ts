65b5ed31859496d597416241e59e18e629cd1231
Omar Abdullah <omar.abdallah5209@gmail.com>
2025-08-30 22:49:25 +0300

    Remove secret

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -7,8 +7,15 @@ if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
   console.error('Missing NEXT_PUBLIC_SUPABASE_ANON_KEY');
 }
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? 'https://bgvyusksldllhwmbbuek.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJndnl1c2tzbGRsbGh3bWJidWVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU4NjMwOTcsImV4cCI6MjA3MTQzOTA5N30.uc2gma8GVXUOnp_p0SXC-Llpk29dhDKR8_5oz7AHfh4';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl) {
+  throw new Error('Missing NEXT_PUBLIC_SUPABASE_URL');
+}
+if (!supabaseAnonKey) {
+  throw new Error('Missing NEXT_PUBLIC_SUPABASE_ANON_KEY');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
